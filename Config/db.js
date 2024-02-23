fe221f62a22c89bd2c55ac4a7c4afac907ef7067
Hemant142 <yhemant203@gmail.com>
2024-02-23 15:58:18 +0530

    update

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
 
-const connection = mongoose.connect(process.env.mongoURL)
+// Load environment variables from .env file
+require("dotenv").config();
 
-module.exports = connection
+// Get the MongoDB connection URI from environment variables
+const dbURI = process.env.MONGODB_URI;
+
+// Connect to MongoDB
+const connection = mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = connection;
